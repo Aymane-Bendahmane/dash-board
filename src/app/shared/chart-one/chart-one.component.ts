2aba6744170a15e16663a5d8aef1be83d31e9b1a
Aymane <aymane.bendahmane14@gmail.com>
2021-12-06 00:40:18 +0100

    dfgdfg

---
+++ src/app/shared/chart-one/chart-one.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+import { Chart } from 'chart.js';
+
+
+@Component({
+  selector: 'app-chart-one',
+  templateUrl: './chart-one.component.html',
+  styleUrls: ['./chart-one.component.scss']
+})
+export class ChartOneComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/shared/chart-two/chart-two.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import {Chart} from 'chart.js';
+
+@Component({
+  selector: 'app-chart-two',
+  templateUrl: './chart-two.component.html',
+  styleUrls: ['./chart-two.component.scss']
+})
+export class ChartTwoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
